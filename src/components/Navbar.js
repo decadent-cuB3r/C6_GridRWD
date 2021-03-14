362300723337eaff7c56b652c168de987ffb700e
decadent-cuB3r <cub3r.ezra.yu@gmail.com>
2021-03-14 19:15:22 +0800

    Navbar
    addition of Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+export default function Navbar() {
+  return (
+    <ul className="Navbar">
+      <li className="Navbar-item">
+        <a href="#"> Tableware </a>
+      </li>
+      <li className="Navbar-item">
+        <a href="#"> Cookware </a>
+      </li>
+      <li className="Navbar-item">
+        <a href="#"> Home accessories </a>
+      </li>
+      <li className="Navbar-item">
+        <a href="#"> Lighting </a>
+      </li>
+      <li className="Navbar-item">
+        <a href="#"> Textile </a>
+      </li>
+      <li className="Navbar-item">
+        <a href="#"> Furniture </a>
+      </li>
+    </ul>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,17 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import ProductList from "../components/ProductList";
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
     <div className="bg-color">
-        <div className="container grid-container">
-            <Header />
-            <ProductList />
-            <Footer />
-        </div>
+      <div className="container-fluid grid-container">
+        <Header />
+        <Navbar />
+        <ProductList />
+        <Footer />
+      </div>{" "}
     </div>
   );
 }
